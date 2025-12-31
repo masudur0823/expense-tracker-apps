278c3f053d3ae0bd6f210cdd8e3c6d77f56afa07
masudur0823 <masudur0823@gmail.com>
2025-12-31 22:37:31 +0600

    e

--- src/app/_lib/api/axios.js
+++ src/app/_lib/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const BASE_URL = process.env.DOMAIN;
+const BASE_URL = process.env.DOMAIN;
 // const BASE_URL = 'http://192.168.3.183:3000'; // LOCAL server
-const BASE_URL = 'https://expense-tracker-marjan.netlify.app'; // LIVE server
+//const BASE_URL = 'https://expense-tracker-marjan.netlify.app'; // LIVE server
 
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.headers.common['Cross-Origin-Resource-Policy'] = 'cross-origin';
