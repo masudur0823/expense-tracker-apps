18a875e122228c0291b636aa6bb649acb9dd86be
masudur0823 <masudur0823@gmail.com>
2026-02-23 20:07:05 +0600

    e

--- src/app/_components/AddExpenseModal.jsx
+++ src/app/_components/AddExpenseModal.jsx
@@ -27,94 +27,13 @@ import {
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import RestaurantIcon from "@mui/icons-material/Restaurant";
-import TapasIcon from "@mui/icons-material/Tapas";
-import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
-import BusinessIcon from "@mui/icons-material/Business";
-import MedicalServicesIcon from "@mui/icons-material/MedicalServices";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import ReceiptIcon from "@mui/icons-material/Receipt";
-import HomeIcon from "@mui/icons-material/Home";
-import SpaIcon from "@mui/icons-material/Spa";
-import LocalActivityIcon from "@mui/icons-material/LocalActivity";
-import SchoolIcon from "@mui/icons-material/School";
-import CategoryIcon from "@mui/icons-material/Category";
 import { useTheme } from "@mui/material/styles";
 import { parseExpenses } from "./expense/utils";
 import dayjs from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-
-const CATEGORIES = [
-  {
-    value: "food",
-    label: "Food & Dining",
-    color: "#FF6B6B",
-    icon: <RestaurantIcon />,
-  },
-  {
-    value: "snacks",
-    label: "Snacks",
-    color: "#416b1a",
-    icon: <TapasIcon />,
-  },
-  {
-    value: "transport",
-    label: "Transportation",
-    color: "#4ECDC4",
-    icon: <DirectionsCarIcon />,
-  },
-  {
-    value: "office",
-    label: "Office",
-    color: "#0c3342",
-    icon: <BusinessIcon />,
-  },
-  {
-    value: "health",
-    label: "Health & Medical",
-    color: "#EF476F",
-    icon: <MedicalServicesIcon />,
-  },
-  {
-    value: "shopping",
-    label: "Shopping",
-    color: "#FFD166",
-    icon: <ShoppingCartIcon />,
-  },
-  {
-    value: "bills",
-    label: "Bills & Utilities",
-    color: "#118AB2",
-    icon: <ReceiptIcon />,
-  },
-  {
-    value: "bari",
-    label: "Kazi Bari",
-    color: "#1d5a4a",
-    icon: <HomeIcon />,
-  },
-  {
-    value: "care",
-    label: "Personal Care",
-    color: "#01467e",
-    icon: <SpaIcon />,
-  },
-  {
-    value: "entertainment",
-    label: "Entertainment",
-    color: "#06D6A0",
-    icon: <LocalActivityIcon />,
-  },
-  {
-    value: "education",
-    label: "Education",
-    color: "#7209B7",
-    icon: <SchoolIcon />,
-  },
-  { value: "other", label: "Other", color: "#6C757D", icon: <CategoryIcon /> },
-];
+import CATEGORIES from "../staticData/category";
 
 const Transition = (props) => <Slide direction="up" {...props} />;
 
